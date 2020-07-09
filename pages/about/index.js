86cf1d6b3869b9c0c6af733306ec0c51373a5b9a
Nadimpalli <snadimpa@FTDCORP.NET>
2020-07-09 12:03:19 +0530

    Axio Support

--- pages/about/index.js
+++ pages/about/index.js
@@ -1,6 +1,31 @@
-import Link from 'next/link'
+import Link from 'next/link';
+import React, { useEffect } from 'react';
+import request from '../../library/js/modules/request';
 
 export function About() {
+
+    useEffect( () => {
+
+        debugger;
+
+        request.get( 'zip', {
+            params: {}
+        } ).then( ( res )=>{
+    
+            console.log('sample get call');
+            //return '';
+    
+        } )
+        .catch( ( err ) => {
+            debugger;
+    
+            console.log('Exception on get call');
+            //return '';
+    
+        } );
+
+    }, [] );
+
     return ( 
         <div>
             <h1>This is About page</h1>
